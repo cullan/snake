a8bc7dd7edf59496d8129d610bf1d4718d3c6d41
Cullan Springstead <cullan@springstead.me>
2018-10-27 15:55:13 -0500

    move board height and width to a vector in game state

--- src/cljs/snake/components.cljs
+++ src/cljs/snake/components.cljs
@@ -3,6 +3,7 @@
 
 (def snake-head-color "#366")
 (def snake-tail-color "#9FF")
+(def food-color "#C00")
 
 (defn snake-part [[x y] color]
   [:rect {:x (+ (* x 10) 1)
@@ -19,10 +20,10 @@
        ^{:key (str "snake-" x "-" y)} [snake-part [x y] snake-tail-color])]))
 
 (defn board [game-state]
-  (let [width (r/cursor game-state [:board-width])
-        height (r/cursor game-state [:board-height])]
+  (let [dimensions (r/cursor game-state [:board-dimensions])
+        [height width] @dimensions]
     [:div {:class "board-container"}
      [:svg {:class "board"
-            :view-box (str "0 0 " (* @width 10) " " (* @height 10))}
+            :view-box (str "0 0 " (* width 10) " " (* height 10))}
       [:rect {:width "100%" :height "100%" :fill "#777"}]
       [snake (r/cursor game-state [:snake])]]]))
--- src/cljs/snake/game.cljs
+++ src/cljs/snake/game.cljs
@@ -2,8 +2,7 @@
   (:require [clojure.spec.alpha :as s :include-macros true]
             [reagent.core :as r]))
 
-(s/def ::board-height int?)
-(s/def ::board-width int?)
+(s/def ::board-dimensions (s/tuple int? int?))
 (s/def ::position (s/tuple int? int?))
 (s/def ::snake (s/coll-of ::position :min-count 1 :distinct true))
 (s/def ::game-state-spec (s/keys :req-un [::board-height
@@ -11,13 +10,8 @@
                                           ::snake]))
 
 (def initial-game-state
-  {:board-height 50
-   :board-width 50
-   :snake [[25 25] [25 24] [25 23] [24 23]]
-   :snake-direction :down})
+  {:board-dimensions [50 50]
+   :snake [[25 25] [25 24] [25 23] [24 23]])
 
 (def game-state (r/atom initial-game-state))
 (set-validator! game-state (partial s/valid? ::game-state-spec))
-
-(defn move-snake [snake direction]
-  snake)
